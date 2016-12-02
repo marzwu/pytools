500fe36bb2d06e42f451ccd43359398289e7c43c
wudan <wudan@shinezone.com>
2016-12-02 10:04:04 +0800

    add unique

---
+++ uniqueAndSort.py
@@ -0,0 +1,30 @@
+blockIds = [20,
+            21,
+            22,
+            23,
+            12,
+            13,
+            14,
+            15, 34,
+            35,
+            42,
+            43,
+            50,
+            51, 10,
+            18,
+            26,
+            11,
+            19,
+            27, 10,
+            11,
+            12,
+            20,
+            21,
+            22,
+            23]
+print(blockIds)
+
+_set = set(blockIds)
+blockIds = list(_set)
+blockIds = sorted(blockIds)
+print(blockIds)
