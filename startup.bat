b3cb99949da3fc125f22a6e799d79c80b5cfd42a
wudan <wudanhit@163.com>
2016-12-23 09:09:43 +0800

    + 系统启动后更新svn，启动ide们

---
+++ startup.bat
@@ -0,0 +1,20 @@
+cd E:\workspace\SAClient\mainVersion
+svn up
+
+cd E:\workspace\SAServer\mainVersion
+svn up
+
+cd E:\workspace\SAServer\mainVersion\bin\dat
+svn up
+
+start "" "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.exe"
+
+start "" "D:\Program Files (x86)\JetBrains\CLion 2016.3\bin\clion64.exe"
+
+start "" "D:\Program Files (x86)\JetBrains\IntelliJ IDEA 2016.3\bin\idea.exe"
+
+start "" "D:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe"
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+start "" "D:\Program Files\Sublime Text 3\sublime_text.exe"
