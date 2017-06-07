e0bf1bb4f859356ba1cc8968769e13c83ac57641
wudan <wudanhit@163.com>
2017-06-07 09:24:31 +0800

    update

--- start_apps.bat
+++ start_apps.bat
@@ -1,11 +1,11 @@
+start "" C:\Users\DELL\AppData\Local\SourceTree\Update.exe --processStart "SourceTree.exe"
+
+start "" "C:\Users\DELL\AppData\Local\Google\Chrome\Application\chrome.exe"
+
 start "" "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.exe"
-
-start "" "D:\Program Files (x86)\JetBrains\Gogland 163.10615.22\bin\gogland.exe"
-
-start "" "D:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe"
-
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
 start "" "D:\Program Files\Sublime Text 3\sublime_text.exe"
 
-start "" "D:\Program Files (x86)\JetBrains\PyCharm 2016.3.1\bin\pycharm.exe"
+start "" "D:\Program Files (x86)\JetBrains\IntelliJ IDEA 2017.1\bin\idea64.exe"
+
+start "" "D:\Program Files (x86)\JetBrains\PyCharm 2016.3.1\bin\pycharm64.exe"
